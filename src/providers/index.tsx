390d1295097fc2aec0fa32e08c94203a133868d8
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-09-10 12:07:28 +0600

    tanstack provider setup done

---
+++ src/providers/index.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { ReactNode } from 'react';
+import QueryProvider from './query.provider';
+
+const Providers = ({children} : {children : ReactNode}) => {
+    return (
+        <QueryProvider>
+            {children}
+        </QueryProvider>
+    );
+};
+
+export default Providers;
