66d7adb240684369a56dc9123e29c87c023baeea
Rafi-Shariar <rafi.shariar619@gmail.com>
2026-09-10 22:42:26 +0600

    login & logout completed

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -4,3 +4,11 @@ import { ILoginUserPayload } from "@/types/auth.type";
 export const userLogin = (payload: ILoginUserPayload) => {
   return apiClient("/api/v1/auth/login", { method: "POST", body: payload });
 };
+
+export const userLogout = () => {
+  return apiClient("/api/v1/auth/logout", { method: "POST" });
+};
+
+export const getMe = () => {
+  return apiClient("/api/v1/auth/me");
+};
--- src/components/layout/public/Header.tsx
+++ src/components/layout/public/Header.tsx
@@ -1,21 +1,77 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useGetMe, useLogout } from "@/hooks";
+import { useQueryClient } from "@tanstack/react-query";
 import Link from "next/link";
 import React from "react";
+import { toast } from "sonner";
 
 const Header = () => {
+
+  const {data, isLoading} = useGetMe()
+  const {mutate: logout} = useLogout()
+  const queryClient = useQueryClient()
+
+  const handleLogout = () => {
+    logout(undefined, {
+      onSuccess: () => {
+        toast.success("Logout successful.")
+        queryClient.removeQueries({queryKey : ['user']})
+      },
+      onError : ()=>{
+        toast.error("Something went wrong. Try again.")
+      }
+    })
+
+  }
+
+
   const routes = [
     { name: "Home", url: "/" },
     { name: "About us", url: "/about-us" },
     { name: "Contact", url: "/contact" },
-    { name: "login", url: "/login" },
   ];
   return (
-    <div className="w-full h-16 border-b flex justify-center items-center gap-6">
-      {routes.map((route) => (
+    <div className="w-full h-16 border-b gap-6">
+
+     <div className="max-w-7xl mx-auto flex justify-between items-center">
+       <div>
+          <h1>ADVISO</h1>
+       </div>
+       <div className="space-x-6">
+        {routes.map((route) => (
         <Link key={route.name} href={route.url}>
           {" "}
           {route.name}
         </Link>
       ))}
+
+       </div>
+       <div>
+        {
+          !isLoading && !data && (
+            <Link href={'/login'}>
+        <Button>
+          Login
+        </Button>
+        </Link>
+          )
+        }
+
+         {
+          !isLoading && data && (
+            
+        <Button onClick={handleLogout}>
+          Logout
+        </Button>
+       
+          )
+        }
+        
+       </div>
+     </div>
+
+    
     </div>
   );
 };
